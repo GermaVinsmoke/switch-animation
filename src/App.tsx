7459758fe01beaa62b85031748451762871898ec
GermaVinsmoke <vaibhav1180@gmail.com>
2022-04-08 23:14:35 +0530

    added state, ref and toggle
    Signed-off-by: GermaVinsmoke <vaibhav1180@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const CIRCLE_WIDTH = 40;
   const SWITCH_WIDTH = 100;
+  const [switchState, setSwitchState] = useState('off');
+  const circleRef = useRef(null);
+  const switchRef = useRef(null);
+  const labelRef = useRef(null);
+
+  const handleClick = () => {
+    if (switchState === 'on') setSwitchState('off');
+    else setSwitchState('on');
+  };
 
   return (
     <div className="background">
-      <div className="switch-container" style={{ width: SWITCH_WIDTH }}>
-        <div className="switch-circle" style={{ width: CIRCLE_WIDTH }}></div>
+      <div
+        className="switch-container"
+        onClick={handleClick}
+        ref={switchRef}
+        style={{ width: SWITCH_WIDTH }}
+      >
+        <div
+          className="switch-circle"
+          ref={circleRef}
+          style={{ width: CIRCLE_WIDTH }}
+        ></div>
       </div>
-      <span className="label">OFF</span>
+      <span className="label" ref={labelRef}>
+        {switchState === 'on' ? 'N' : 'FF'}
+      </span>
     </div>
   );
 }

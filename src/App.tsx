4b80cb9ccfc23d5be884b4cd4b3ca82803be7b2d
GermaVinsmoke <vaibhav1180@gmail.com>
2022-04-08 23:16:41 +0530

    added gsap animation, removed some css
    Signed-off-by: GermaVinsmoke <vaibhav1180@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { gsap, Elastic } from 'gsap';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,6 +9,62 @@ function App() {
   const circleRef = useRef(null);
   const switchRef = useRef(null);
   const labelRef = useRef(null);
+
+  const t1 = gsap.timeline();
+
+  useEffect(() => {
+    if (switchState === 'on') {
+      t1.to(
+        circleRef.current,
+        {
+          duration: 0.5,
+          x: SWITCH_WIDTH - CIRCLE_WIDTH,
+          backgroundColor: '#51ff0d',
+          ease: Elastic.easeOut.config(1, 0.6),
+        },
+        'start'
+      )
+        .to(
+          switchRef.current,
+          {
+            duration: 0.5,
+            borderColor: '#51ff0d',
+          },
+          'start'
+        )
+        .fromTo(
+          labelRef.current,
+          { y: 20, opacity: 0, color: 'white' },
+          { y: 0, opacity: 1, color: '#51ff0d', duration: 0.5 },
+          'start'
+        );
+    } else {
+      t1.to(
+        circleRef.current,
+        {
+          duration: 0.5,
+          x: 0,
+          backgroundColor: '#fff',
+          ease: Elastic.easeOut.config(1, 0.6),
+        },
+        'reverse'
+      )
+        .to(
+          switchRef.current,
+          {
+            duration: 0.5,
+            borderColor: '#fff',
+          },
+          'reverse'
+        )
+        .fromTo(
+          labelRef.current,
+          { y: 20, opacity: 0, color: 'white' },
+          { y: 0, opacity: 1, color: 'white', duration: 0.5 },
+          'reverse'
+        );
+    }
+  }, [switchState]);
 
   const handleClick = () => {
     if (switchState === 'on') setSwitchState('off');
